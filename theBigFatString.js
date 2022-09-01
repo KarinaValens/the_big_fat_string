39af9768f153d5ca785e8d3560529e49fc098713
Karina Valens <karyvalens@yahoo.com>
2022-09-01 19:26:50 +0200

    beginning...

--- theBigFatString.js
+++ theBigFatString.js
@@ -48,27 +48,39 @@ function inputFunt() {
         case "4":
             chosen === 4;
             //4.- If input is a full name: Find the middle name start and end position, and the middle name itself in a fullname string
-            outputfield.value = (`${inputfield.indexOf(" ")+1} - ${inputfield.lastIndexOf(" ")-1} = ${inputfield.substring((inputfield.indexOf(" ")+1),inputfield.lastIndexOf(" "))}`);
+            outputfield.value = (`${inputfield.indexOf(" ")+1} - ${inputfield.lastIndexOf(" ")-1} = 
+            ${inputfield.substring((inputfield.indexOf(" ")+1),inputfield.lastIndexOf(" "))}`);
+            break
+        case "5":
+            chosen === 5;
+            //5.- If input is a filename: Check if a filename is .png or .jpg
+            outputfield.value = inputfield.endsWith(`.png`) || inputfield.endsWith(`.jpg`);
+            break
+        case "6":
+            chosen === 6;
+            //6.- If input is a password: Hide a password with the correct number of *s
+            outputfield.value = ("*").repeat(inputfield.length);
+            break
+        case "7":
+            chosen === 7;
+            //7.- With any input: Make the third character uppercase
+            outputfield.value = inputfield[3].toUpperCase()
+            //1.- If input is a first name: Make the first character in a name uppercase, and the rest lowercase
+            outputfield.value = replace(inputfield[2], inputfield[2].toUpperCase());
+            //can i use an array method here?
+            break
+        case "8":
+            chosen === 8;
+            //8.- With any input: Make a character uppercase, if it follows a space or a hyphen
+            //outputfield.value = inputfield.substring(0, inputfield.indexOf(" ")).length;
+
             break
     }
-    /*             
-   
-    <option value="4"></option>
-    <option value="5">5.- If input is a filename: Check if a filename is .png or .jpg</option>
-    <option value="6">6.- If input is a password: Hide a password with the correct number of *s</option>
-    <option value="7">7.- With any input: Make the third character uppercase</option>
-    <option value="8">8.- With any input: Make a character uppercase, if it follows a space or a hyphen</option>
- */
+
 }
 
 
 
-
-//note: parseInt(#formatSelector.options[formatselemmmmm id])
-//console.log("name in string", theConst)
-//using switch instead of if else statements with cases for the options case 1,2 etc + break so the next case doesnr run inmidiately
-
-//const.substring(o,imputValue.indexOf" ")
 
 //the last item is solfe with for loop
 
